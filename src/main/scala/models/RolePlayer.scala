128d1557ad0c4d665b57324767b1b10239c43a9b
James Forster <james.forster@digital.hmrc.gov.uk>
2016-10-20 14:35:08 +0100

    Added new constructor for RolePlayer from a player.

--- src/main/scala/models/RolePlayer.scala
+++ src/main/scala/models/RolePlayer.scala
@@ -1,7 +1,10 @@
 package models
 
+import controllers.PlayerController
 import org.bukkit.entity.Player
 import models.PlayerJob._
+
+import scala.util.Success
 
 /**
   * Created by james-forster on 31/08/16.
@@ -9,7 +12,25 @@ import models.PlayerJob._
 
 case class RolePlayer (player: Player, activeJob: PlayerJob, allJobs: Seq[PlayerJob])
 
-object RolePlayer {
+object RolePlayer extends RolePlayerMethods {
+
+  val playerController = PlayerController
+
+}
+
+trait RolePlayerMethods {
+
+  val playerController: PlayerController
+
+  def apply(player: Player): RolePlayer = {
+    val activeJob = playerController.getActivePlayerJob(player)
+    val allJobs = playerController.getAllPlayerJobs(player)
+
+    (activeJob, allJobs) match {
+      case (Success(job), Success(jobs)) => RolePlayer(player, job, jobs)
+      case _ => RolePlayer(player, PlayerJob(Jobs.Jobless, 0, 0), Seq())
+    }
+  }
 
   implicit val stringToAllJobs: String => Seq[PlayerJob] = input => {
     val jobsArrayString = input.split(" ")
--- src/test/scala/models/RolePlayerSpec.scala
+++ src/test/scala/models/RolePlayerSpec.scala
@@ -1,24 +1,48 @@
 package models
 
+import common.UnitSpec
+import controllers.PlayerController
 import org.bukkit.entity.Player
+import org.mockito.Matchers
+import org.mockito.Mockito._
 import org.scalatest.mock.MockitoSugar
-import org.scalatest.{Matchers, OptionValues, WordSpecLike}
+
+import scala.util.{Failure, Success}
 
 /**
   * Created by james-forster on 08/09/16.
   */
-class RolePlayerSpec extends AnyRef with WordSpecLike with Matchers with OptionValues with MockitoSugar {
+class RolePlayerSpec extends UnitSpec with MockitoSugar {
 
-  def setupTarget(activeJob: PlayerJob, allJobs: Seq[PlayerJob]): RolePlayer = {
+  val exception = new Exception
+  val job = PlayerJob(Jobs.Rogue, 1, 1)
+
+  def setupRolePlayer(activeJob: PlayerJob, allJobs: Seq[PlayerJob]): RolePlayer = {
 
     val mockPlayer = mock[Player]
 
     new RolePlayer(mockPlayer, activeJob, allJobs)
   }
 
+  def setupRolePlayerMethods(newPlayer: Boolean) : RolePlayerMethods = {
+
+    val mockPlayerController = mock[PlayerController]
+
+    when(mockPlayerController.getActivePlayerJob(Matchers.any()))
+      .thenReturn(if (newPlayer) Failure(exception) else Success(job))
+
+    when(mockPlayerController.getAllPlayerJobs(Matchers.any()))
+      .thenReturn(if (newPlayer) Failure(exception) else Success(Seq(job)))
+
+    new RolePlayerMethods {
+      override val playerController: PlayerController = mockPlayerController
+    }
+  }
+
+
   "A player with only a single warrior job" which {
     val warrior = PlayerJob(Jobs.Warrior, 5, 200)
-    val target = setupTarget(warrior, Seq(warrior))
+    val target = setupRolePlayer(warrior, Seq(warrior))
 
     "calls allJobsToString" should {
       val result = RolePlayer.allJobsToString(target.allJobs)
@@ -42,7 +66,7 @@ class RolePlayerSpec extends AnyRef with WordSpecLike with Matchers with OptionV
     val warrior = PlayerJob(Jobs.Warrior, 5, 200)
     val wizard = PlayerJob(Jobs.Wizard, 1, 0)
     val jobless = PlayerJob(Jobs.Jobless, 0, 0)
-    val target = setupTarget(warrior, Seq(warrior, wizard, jobless))
+    val target = setupRolePlayer(warrior, Seq(warrior, wizard, jobless))
 
     "calls allJobsToString" should {
       val result = RolePlayer.allJobsToString(target.allJobs)
@@ -62,4 +86,22 @@ class RolePlayerSpec extends AnyRef with WordSpecLike with Matchers with OptionV
     }
   }
 
+  "Creating a RolePlayer from a player" should {
+
+    "create a valid RolePlayer if the player already exists" in {
+      val rolePlayer = setupRolePlayerMethods(false)
+      val mockPlayer = mock[Player]
+      val result = rolePlayer.apply(mockPlayer)
+
+      result shouldBe RolePlayer(mockPlayer, job, Seq(job))
+    }
+
+    "create a new RolePlayer if the player is not found" in {
+      val rolePlayer = setupRolePlayerMethods(true)
+      val mockPlayer = mock[Player]
+      val result = rolePlayer.apply(mockPlayer)
+
+      result shouldBe RolePlayer(mockPlayer, PlayerJob(Jobs.Jobless, 0, 0), Seq())
+    }
+  }
 }
